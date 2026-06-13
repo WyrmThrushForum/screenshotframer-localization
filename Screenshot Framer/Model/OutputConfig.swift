93b164c7aba6d1d319af2510b19ad4ba2ad630f9
WyrmThrushForum <vildanozgorkey77349@gmail.com>
2026-06-12 20:46:56 -0500

    Create Screenshot Framer/Model/OutputConfig.swift

---
+++ Screenshot Framer/Model/OutputConfig.swift
@@ -0,0 +1,42 @@
+//
+//  OutputConfig.swift
+//  Screenshot Framer
+//
+//  Created by Patrick Kladek on 19.12.17.
+//  Copyright © 2017 Patrick Kladek. All rights reserved.
+//
+
+import Foundation
+
+
+struct OutputConfig: Codable {
+
+    // MARK: - Properties
+
+    let transparent: Bool?
+    let output: String
+    let fromImageNumber: Int
+    let toImageNumber: Int
+}
+
+extension OutputConfig {
+
+    func prefered(from: Int?) -> Int? {
+        guard let from = from else { return nil }
+
+        if from >= self.fromImageNumber && from <= self.toImageNumber {
+            return from
+        }
+
+        return nil
+    }
+
+    func prefered(end: Int?) -> Int? {
+        guard let end = end else { return nil }
+
+        if end >= self.fromImageNumber && end <= self.toImageNumber {
+            return end
+        }
+        return nil
+    }
+}
